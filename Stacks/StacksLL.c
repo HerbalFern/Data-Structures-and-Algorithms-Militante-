73454ddf895847c5e6840e5c80b0857176fcc858
HerbalFern <rosellofernandrei@gmail.com>
2025-09-09 00:34:34 +0800

    StacksLL done

--- Stacks/StacksLL.c
+++ Stacks/StacksLL.c
@@ -8,50 +8,77 @@ typedef struct node {
 
 typedef struct {
     Node* head;
+    int count;
 } Stack;
 
-void initList(Stack);
+void initList(Stack*);
 
 void push(Stack*, int);
 void pop(Stack*);
 void peek(Stack);
 int isEmpty(Stack);
 
-int main(){
+void printStack(Stack stack);
 
+int main(){
+    Stack stack;
+    initList(&stack);
+    push(&stack, 4);
+    push(&stack, 3);
+    push(&stack, 2);
+
+    peek(stack);
+
+    push(&stack, 1);
+    pop(&stack);
+
+    printStack(stack);
 
     return 0;
 }
 
 void push(Stack* stack, int data){
     Node* temp = (Node*) malloc(sizeof(Node));
+    Node** head = &stack->head;
     if (temp != NULL) {
         temp->data = data;
-        temp->next = stack->head;
-        stack->head = temp;
+        temp->next = *head;
+        *head = temp;
         printf("Successfully Pushed %d to Stack\n", data);
-    }
+        stack->count++;
+    } 
 }
 
 void pop(Stack* stack){
     
-    if (stack->head != NULL){
-        Node* head;
-        head = stack->head;
-        Node* temp = head;
-        head = head->next;
+    if (isEmpty(*stack) == 0){
+        Node* temp = stack->head;
+        
+        printf("Popping %d from the Stack\n", temp->data);
+        
+        stack->head = stack->head->next;
         free(temp);
-    } else {
-        printf("List is empty\n");
     }
 }
 
 void peek(Stack stack){
-    printf("Top: %d", stack.head->data);
+    (stack.count > 0) ? printf("Top: %d\n", stack.head->data) : printf("Stack is empty. \n");
 }
 
 int isEmpty(Stack stack){
     if(stack.head == NULL){
-        
-    }
+        return 1;
+    } return 0;
 }
+
+void initList(Stack* stack){
+    stack->count = 0;
+    stack->head = NULL;
+}
+
+void printStack(Stack stack){
+    Node* trav;
+    for (trav = stack.head; trav != NULL; trav = trav->next){
+        printf("| %d |\n", trav->data);
+    } printf("=====\n");
+}   
