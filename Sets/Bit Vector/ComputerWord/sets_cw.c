17f587ac1be699f1ca3180a8a47999d7b1c1124b
HerbalFern <rosellofernandrei@gmail.com>
2025-10-26 15:04:15 +0800

    moved some files

--- Sets/Bit Vector/ComputerWord/sets_cw.c
+++ Sets/Bit Vector/ComputerWord/sets_cw.c
@@ -15,9 +15,65 @@ void display(SET set);
 
 int main(){
 
-    // add main
+    SET A, B, C;
+    initialize(&A);
+    initialize(&B);
+
+    // --- TEST 1: INSERT ELEMENTS ---
+    // Insert elements into A: 0, 2, 4
+    insert(&A, 0);
+    insert(&A, 2);
+    insert(&A, 4);
+
+    // Insert elements into B: 1, 2, 3, 5
+    insert(&B, 1);
+    insert(&B, 2);
+    insert(&B, 3);
+    insert(&B, 5);
+
+    printf("Set A elements: ");
+    display(A);
+    printf("\n");
+
+    printf("Set B elements: ");
+    display(B);
+    printf("\n");
+
+    // --- TEST 2: FIND ELEMENTS ---
+    // Check if 2 is in A
+    printf("Find 2 in A: %s\n", find(&A, 2) ? "true" : "false");
+    // Check if 3 is in A
+    printf("Find 3 in A: %s\n", find(&A, 3) ? "true" : "false");
+
+    // --- TEST 3: UNION ---
+    C = Union(A, B);
+    printf("Union of A and B: ");
+    display(C);
+    printf("\n");
+
+    // --- TEST 4: INTERSECTION ---
+    C = intersection(A, B);
+    printf("Intersection of A and B: ");
+    display(C);
+    printf("\n");
+
+    // --- TEST 5: DIFFERENCE ---
+    C = difference(A, B);
+    printf("Difference of A - B: ");
+    display(C);
+    printf("\n");
+
+    // --- TEST 6: DELETE ELEMENTS ---
+    delete(&A, 2);
+    printf("A after deleting element 2: ");
+    display(A);
+    printf("\n");
 
     return 0;
+}
+
+void initialize(SET* set){
+    *set = 0;
 }
 
 
@@ -53,7 +109,7 @@ void display(SET set){
     int mask = 1; 
     for (int i = 0; i < 8; i++){
         if ((set & (mask << i)) != 0){
-            printf("%d ");
+            printf("%d ", i);
         } 
     }
 }
